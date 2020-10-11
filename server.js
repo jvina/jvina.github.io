6bb6aec5ff44cc1c0b661fe5a32227e78efdfba5
Jilliane Vina <jvina@my.bcit.ca>
2020-10-10 17:13:12 -0700

    testing heroku app

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let dt = require('./myModule');
 
 http.createServer(function (req, res) {
     let q = url.parse(req.url, true);
+    console.log(q.query);
     res.writeHead(200, {"Content-type":"text/html","Access-Control-Allow-Origin": "*"});
     res.end('Hello ' + q.query["name"] + ", the current server time is " + dt.myDateTime());
-}).listen(8888);
+}).listen(process.env.PORT || 8888);
